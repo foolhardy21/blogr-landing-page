a7a61d18ca8d3199b90e28b0e3c6ad7489bfda55
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-06-10 15:03:30 +0530

    hamburger-icon-functionality-added

---
+++ script.js
@@ -0,0 +1,29 @@
+const hamburgerLogo = document.createElement('img')
+
+const maxWidth = window.matchMedia('(max-width: 600px)')
+maxWidth.addListener(addHamburger)
+addHamburger(maxWidth)
+
+const minWidth = window.matchMedia('(min-width: 601px)')
+minWidth.addListener(removeHamburger)
+removeHamburger(minWidth)
+
+function addHamburger(width) {
+  if(width.matches) {
+
+    const logoBar = document.querySelector('[comp-detail]')
+    hamburgerLogo.src = './images/icon-hamburger.svg'
+
+    logoBar.appendChild(hamburgerLogo)
+
+  }
+
+}
+
+function removeHamburger(width) {
+  if(width.matches) {
+    console.log(hamburgerLogo)
+    hamburgerLogo.remove()
+  }
+
+}
